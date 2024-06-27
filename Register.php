fb1357600a1211bb92b3f99824cd546ac8db17c9
azizah <azizah@gmail.com>
2024-06-27 14:14:56 +0700

    ini yang fix nya suda memakai template

--- Register.php
+++ Register.php
@@ -55,11 +55,11 @@
   <!-- /.register-box -->
 
   <!-- jQuery -->
-  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <script src="app/plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
-  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="../../dist/js/adminlte.min.js"></script>
+  <script src="app/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
